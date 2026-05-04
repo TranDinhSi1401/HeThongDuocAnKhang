51d8c0595cdb6056c6dd91865d0a166df7b14afc
Quan <23655101.quan@student.iuh.edu.vn>
2026-05-05 00:37:17 +0700

    lưu cấu trúc gui vừa thêm

--- src/main/java/server/entity/SanPham.java
+++ src/main/java/server/entity/SanPham.java
@@ -51,8 +51,6 @@ public class SanPham {
     @OneToMany(mappedBy = "sanPham", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<LoSanPham> loSanPhams;
 
-    @Column(name = "da_xoa")
-    private boolean daXoa;
 
     @OneToMany(mappedBy = "sanPham", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<DonViTinh> donViTinhs;
